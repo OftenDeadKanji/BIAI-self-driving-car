7267d5cdd5f867fa815874239d7f1093c3e3cf95
Mateusz Chłopek <32665400+OftenDeadKanji@users.noreply.github.com>
2020-04-22 14:35:13 +0200

    22 kwi 2020
    Dodanie toru, podążanie kamery za autem.

---
+++ Car_Unity/Car_BIAI/Assets/Scripts/CarFollower.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class CarFollower : MonoBehaviour
+{
+    public Transform carTransform;
+    public Vector3 cameraOffset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = carTransform.position + cameraOffset;
+    }
+
+    void changeCar()
+    {
+        //TODO - zmiana auta na te z najlepszym wynikiem
+    }
+}
